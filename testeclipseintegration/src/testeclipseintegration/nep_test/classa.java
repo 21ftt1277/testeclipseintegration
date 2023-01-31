b54d79b2f81ecc97bec4901a983b1fdf45cff1c2
Danial Norafiq <21ftt1277@student.pb.edu.bn>
2023-01-31 15:35:00 +0800

    new abc

---
+++ testeclipseintegration/src/testeclipseintegration/nep_test/classa.java
@@ -0,0 +1,19 @@
+package testeclipseintegration.nep_test;
+
+public class classa {
+	
+	private String abc;
+	
+	public classa(String abc) {
+		this.abc = abc;
+	}
+	
+	public String getAbc() {
+		return abc;
+	}
+	
+	public void setAbc(String abc) {
+	this.abc = abc;
+	}
+
+}
--- testeclipseintegration/src/testeclipseintegration/nep_test/nep_test.java
+++ testeclipseintegration/src/testeclipseintegration/nep_test/nep_test.java
@@ -1,5 +1,9 @@
 package testeclipseintegration.nep_test;
 
 public class nep_test {
-
+	public static void main(String[] args) {
+		System.out.println("GitHub");
+		classa classa = new classa("ABCDEF");
+		System.out.println(classa.getAbc());
+	}
 }
